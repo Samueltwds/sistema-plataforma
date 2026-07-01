af6b5f47a756f7fdd4486f83732718ba07fa2230
Samueltwds <samueltwd61@gmail.com>
2026-07-01 10:26:05 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -361,6 +361,7 @@ function renderProgramacoes() {
                   <select value={operador} onChange={e => setOperador(e.target.value)}>
                     <option value="">Selecione o operador</option>
                     <option value="">Selecione o operador. Caso haja mais de um, favor informar todos os nomes no e-mail de confirmação da programação.</option>
+                    <option value="Operador não cadastrado, será informado via e-mail">Operador não cadastrado, será informado via e-mail</option>
                     <option value="Adelmo Ricardo dos Santos - VSB">Adelmo Ricardo dos Santos - VSB</option>
                     <option value="Adriano Paixão de Souza - Progen">Adriano Paixão de Souza - Progen</option>
                     <option value="Ailton Gonçalves Do Carmo Junio - UPTEC">Ailton Gonçalves Do Carmo Junio - UPTEC</option>
